f9035ff796a67d8bd4013e58cb227419ca7bdb71
lucianovilasboas <lucianovilasboas@gmail.com>
2025-09-21 13:08:46 -0300

    updates

--- carbono/Links2.py
+++ carbono/Links2.py
@@ -3,12 +3,13 @@ import streamlit as st
 st.set_page_config(page_title="Fórum França-Brasil", page_icon=":earth_americas:", layout="wide")
 st.title("🔗 Links Úteis")
 
-st.markdown("""
-### Links Importantes
-- [Electricity Maps](https://app.electricitymaps.com/map/24h)
-""")
 
-st.sidebar.title("Navegação")
-st.sidebar.markdown("""
-- [Electricity Maps](https://app.electricitymaps.com/map/24h)
+
+st.subheader("🔗 Fontes e ferramentas úteis")
+st.markdown("""
+- [Electricity Maps](https://app.electricitymaps.com)
+- [ONU - Objetivos de Desenvolvimento Sustentável](https://brasil.un.org/pt-br/sdgs)  
+- [ONU - Mudanças Climáticas](https://www.un.org/climate-change)  
+- [WWF - Calculadora de Pegada de Carbono](https://www.wwf.org.br/natureza_brasileira/reducao_de_impactos2/pegada_ecologica/)  
+- [Global Footprint Network](https://www.footprintnetwork.org/)
 """)
