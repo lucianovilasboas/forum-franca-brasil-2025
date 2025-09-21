5b91f7709e28aa1c76b12b94d451400d86fdff44
lucianovilasboas <lucianovilasboas@gmail.com>
2025-09-21 13:11:49 -0300

    updates

--- carbono/Links2.py
+++ carbono/Links2.py
@@ -13,3 +13,9 @@ st.markdown("""
 - [WWF - Calculadora de Pegada de Carbono](https://www.wwf.org.br/natureza_brasileira/reducao_de_impactos2/pegada_ecologica/)  
 - [Global Footprint Network](https://www.footprintnetwork.org/)
 """)
+
+
+st.subheader("🔗 Links de ONGS e Iniciativas")
+st.markdown("""
+- [Iracambi](https://iracambi.com/pt-br/quem-somos/historia/)
+""")
