ce95262f377fa49e214476c450db21e696627ad9
lucianovilasboas <lucianovilasboas@gmail.com>
2025-10-04 20:02:11 -0300

    update

--- carbono/Links2.py
+++ carbono/Links2.py
@@ -13,10 +13,12 @@ st.markdown("""
 - [WWF - Calculadora de Pegada de Carbono](https://www.wwf.org.br/natureza_brasileira/reducao_de_impactos2/pegada_ecologica/)  
 - [Global Footprint Network](https://www.footprintnetwork.org/)
 - [National Geographic - Antártida Verde](https://www.nationalgeographicbrasil.com/meio-ambiente/2025/08/a-antartida-ja-foi-verde-e-possui-lagos-de-lava-descubra-6-curiosidades-surpreendentes-sobre-o-continente-gelado)
+- [Mata Nativa - Cálculo de Carbono](https://matanativa.com.br/e-possivel-calcular-carbono-com-o-mata-nativa/)
 """)
 
 
-st.subheader("🔗 Links de ONGs e Iniciativas")
+st.subheader("🔗 Eventos, ONGs e outras Iniciativas")
 st.markdown("""
 - [Iracambi](https://iracambi.com/pt-br/quem-somos/historia/)
+- [COP 30](https://cop30.br/en)
 """)
